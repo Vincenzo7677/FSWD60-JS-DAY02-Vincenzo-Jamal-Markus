8fc26e1c91fde615a131c69eed226fef9c3e7c21
Markus Szokoll <markus.szokoll@gmx.at>
2019-01-21 15:29:40 +0100

    atm with text

--- ATM.js
+++ ATM.js
@@ -3,7 +3,7 @@ function atm(a){
 	var note2=Math.floor((a%100)/50);
 	var note3=Math.floor(((a%100)%50)/20);
 	var note4=Math.floor((((a%100)%50)%20)/10);
-	document.write(note1 + note2 + note3 + note4);
+	document.write("100: "+note1 + " 50: "+ note2 + " 20: " + note3 + " 10 " + note4);
 	
 }
 atm(50);
