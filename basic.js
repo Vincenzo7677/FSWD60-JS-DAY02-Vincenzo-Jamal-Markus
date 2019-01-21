e41d42fb00af2e0b1c066b97f233ff0a95c30c84
JamalAhmed2019 <jamalusb@gmx.at>
2019-01-21 10:21:16 +0100

    adding age calculator

--- basic.js
+++ basic.js
@@ -8,3 +8,11 @@ function crystalGazer(numOfChildren, partnersName, geographicLocation, jobTitle)
 	document.write("You will be a "+jobTitle+" in "+geographicLocation+" and married to "+partnersName+" with "+ numOfChildren +" children.");
 	
 }
+function ageCalculator(birthyear, currentyear){
+ var age=currentyear-birthyear;
+ var age2=age-1;                     // -1 because we don't know the birth month
+ document.write("You are either " + age + " or " + age2);
+
+
+}
+ageCalculator(1988,2019);
