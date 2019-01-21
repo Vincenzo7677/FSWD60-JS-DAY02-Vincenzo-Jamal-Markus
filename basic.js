3f1fabbb99c2a131b13503b92073e2975e03e7f5
Markus Szokoll <markus.szokoll@gmx.at>
2019-01-21 09:57:14 +0100

    basic - exercise 1

---
+++ basic.js
@@ -0,0 +1,10 @@
+
+
+crystalGazer(5, "Sonja", "Vienna", "Software Developer");
+
+document.write("text");
+
+function crystalGazer(numOfChildren, partnersName, geographicLocation, jobTitle){
+	document.write("You will be a "+jobTitle+" in "+geographicLocation+" and married to "+partnersName+" with "+ numOfChildren +" children.");
+	
+}
